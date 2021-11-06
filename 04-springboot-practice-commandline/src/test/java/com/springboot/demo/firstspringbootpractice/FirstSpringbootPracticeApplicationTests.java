ad5f3a594fb50d655cd4cc7446ad83567de5f20c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-11-06 17:19:14 +0100

    running application on command line

---
+++ 04-springboot-practice-commandline/src/test/java/com/springboot/demo/firstspringbootpractice/FirstSpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.firstspringbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
