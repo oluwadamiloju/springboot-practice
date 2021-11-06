34aeaa01271477ac9d40ea975b4c4729af568f7d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-11-06 15:16:42 +0100

    added dev tools dependency and tested it

--- 02-springboot-practice-devtools/src/main/java/com/springboot/demo/firstspringbootpractice/restcontroller/FunRestController.java
+++ 02-springboot-practice-devtools/src/main/java/com/springboot/demo/firstspringbootpractice/restcontroller/FunRestController.java
@@ -1,6 +1,5 @@
 package com.springboot.demo.firstspringbootpractice.restcontroller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +7,15 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
-    @Value("${coach.name}")
-    private String coachName;
-    @Value("${team.name}")
-    private String teamName;
-
+    // added endpoint for "/"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!, The time to start being great is " + LocalDateTime.now();
+    }
+
+    // new endpoint for workout page "/workout"
+    @GetMapping("/workout")
+    public String workoutSesh() {
+        return "Our workout for today includes walking 3000 steps, 50 squats and 5 \"1 minute\" planks";
     }
 }
