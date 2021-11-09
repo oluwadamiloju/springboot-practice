578210059f738370eb886bc05e809a47d0b6842e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-11-09 16:47:56 +0100

    played around with some spring data rest properties

--- cruddemo-employee-directory-spring-data-jpa/src/main/java/comspringboot/demo/springbootpractice/demo/dao/EmployeeRepository.java
+++ cruddemo-employee-directory-spring-data-jpa/src/main/java/comspringboot/demo/springbootpractice/demo/dao/EmployeeRepository.java
@@ -2,6 +2,7 @@ package comspringboot.demo.springbootpractice.demo.dao;
 
 import comspringboot.demo.springbootpractice.demo.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
