95a0dac274f54fc34f7cad415dd8372a2729bac4
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-11-09 17:26:55 +0100

    demo with thymeleaf

---
+++ thymeleafdemo/src/main/java/com/springboot/demo/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.springboot.demo.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("date", new java.util.Date());
+        return "helloworld";
+    }
+}
