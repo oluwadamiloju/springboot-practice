b1bde7e0578c2367ef2500c814e784fd8e3f6991
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-03-04 00:19:30 +0100

    beans scope - singleton

---
+++ spring-demo-one/src/com/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,27 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        //load spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        //retrieve beans from spring container
+        Coach trackCoach = context.getBean("trackCoach", Coach.class);
+
+        Coach subCoach =  context.getBean("trackCoach", Coach.class);
+
+        //check if they are same beans
+        boolean result = trackCoach == subCoach;
+        System.out.println("Pointing to the same object is " + result);
+        System.out.println();
+        System.out.println("Memory loaction for trackCoach: " + trackCoach);
+        System.out.println();
+        System.out.println("Memory loaction for subCoach: " + subCoach);
+
+        //close context
+        context.close();
+    }
+}
