5372c19f1f6bc0465198f1c47417143e24bf6962
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-03-04 16:31:14 +0100

    spring config with annotations - IoC

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,30 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// get the bean from spring container
+		Coach tennisCoach = context.getBean("tennisCoach", Coach.class);
+
+		Coach lacrosseCoach = context.getBean("lacrosseCoach", Coach.class);
+
+		// call a method on the bean
+		System.out.println(tennisCoach.getDailyWorkout());
+		System.out.println();
+		System.out.println(lacrosseCoach.getDailyWorkout());
+				
+		// close the context
+		context.close();
+		
+	}
+
+}
+
+
---
+++ spring-demo-annotations/src/com/luv2code/springdemo/LacrosseCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LacrosseCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Do 100 reps of squats and lunges";
+    }
+}
