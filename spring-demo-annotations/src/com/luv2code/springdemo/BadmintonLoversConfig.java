a9c010e1e5f9448048f985dbdbf8ea0de444e0dc
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2022-03-08 10:37:15 +0100

    spring container configuration with java config class - DI and IoC practice activity

--- spring-demo-annotations/src/com/luv2code/springdemo/BadmintonLoversConfig.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/BadmintonLoversConfig.java
@@ -2,7 +2,6 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class BadmintonLoversConfig {
